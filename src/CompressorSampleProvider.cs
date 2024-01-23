8e7e8071e476114fa226476727a221aab0682709
Your Name <you@example.com>
2024-01-23 16:17:08 +0200

    Cleanup in CompressorSampleProvider

--- src/CompressorSampleProvider.cs
+++ src/CompressorSampleProvider.cs
@@ -5,6 +5,9 @@ using NAudio.Dsp;
 
 namespace NAudioEffects
 {
+    /// <summary>
+    /// A sample provider that applies compression to the input audio.
+    /// </summary>
     public class CompressorSampleProvider : EffectSampleProviderBase
     {
         private readonly EnvelopeFollower _envelopeFollower;
@@ -12,6 +15,10 @@ namespace NAudioEffects
         private float _kneeLinear;
         private float _makeupGainLinear;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompressorSampleProvider"/> class.
+        /// </summary>
+        /// <param name="source">The source sample provider.</param>
         public CompressorSampleProvider(ISampleProvider source) 
             : base(source)
         {
@@ -21,12 +28,46 @@ namespace NAudioEffects
             UpdateMakeupGain();
         }
 
+        /// <summary>
+        /// Gets or sets the threshold in decibels.
+        /// </summary>
+        /// <value>The threshold in decibels.</value>
         public float ThresholdDb { get; set; } = -20;
+
+        /// <summary>
+        /// Gets or sets the compression ratio.
+        /// </summary>
+        /// <value>The compression ratio.</value>
         public float Ratio { get; set; } = 4;
+
+        /// <summary>
+        /// Gets or sets the attack time in milliseconds.
+        /// </summary>
+        /// <value>The attack time in milliseconds.</value>
         public float AttackMs { get; set; } = 10;
+
+        /// <summary>
+        /// Gets or sets the release time in milliseconds.
+        /// </summary>
+        /// <value>The release time in milliseconds.</value>
         public float ReleaseMs { get; set; } = 100;
+
+        /// <summary>
+        /// Gets or sets the knee in decibels.
+        /// </summary>
+        /// <value>The knee in decibels.</value>
         public float KneeDb { get; set; } = 6;
+
+        /// <summary>
+        /// Gets or sets the makeup gain in decibels.
+        /// </summary>
+        /// <value>The makeup gain in decibels.</value>
         public float MakeupGainDb { get; set; } = 0;
+
+        /// <summary>
+        /// Gets the current gain reduction in decibels.
+        /// </summary>
+        /// <value>The current gain reduction in decibels.</value>
         public float CurrentGainReductionDb { get; private set; }
 
         private void UpdateThreshold()
@@ -44,6 +85,12 @@ namespace NAudioEffects
             _makeupGainLinear = DbToLinear(MakeupGainDb);
         }
 
+        /// <summary>
+        /// Processes the audio block.
+        /// </summary>
+        /// <param name="buffer">The audio buffer.</param>
+        /// <param name="offset">The offset in the buffer.</param>
+        /// <param name="count">The number of samples to process.</param>
         protected override void ProcessBlock(float[] buffer, int offset, int count)
         {
             _envelopeFollower.Process(buffer, offset, count);
