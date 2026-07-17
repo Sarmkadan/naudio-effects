9ee35a8bc7090b6ca95b39200ad3c53673f0041a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 09:00:04 +0300

    refactor: cleanup in CompressorSampleProviderExtensionsJsonExtensions
    found this while looking at something else.

--- src/CompressorSampleProviderExtensionsJsonExtensions.cs
+++ src/CompressorSampleProviderExtensionsJsonExtensions.cs
@@ -26,11 +26,9 @@ namespace NAudioEffects
         {
             ArgumentNullException.ThrowIfNull(value);
 
-            var options = indented 
-                ? new JsonSerializerOptions(_options) { WriteIndented = true } 
-                : _options;
-                
-            return JsonSerializer.Serialize(value, options);
+            return JsonSerializer.Serialize(value, indented
+                ? new JsonSerializerOptions(_options) { WriteIndented = true }
+                : _options);
         }
 
         /// <summary>
